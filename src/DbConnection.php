30c1ed93243572f67c08a6208df73094c0dc4dda
29575 <2957549@gmail.com>
2024-03-19 23:03:54 +0300

    init commit 1

---
+++ src/DbConnection.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+use PDO;
+use PDOException;
+
+class DbConnection
+{
+    private string $host;
+    private string $dbname;
+    private string $username;
+    private string $password;
+    private string $charset = 'utf8';
+    private array $options = [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    ];
+
+    /**
+     * @param string $host
+     * @param string $dbname
+     * @param string $username
+     * @param string $password
+     */
+    public function __construct(string $host, string $dbname, string $username, string $password)
+    {
+        $this->host = $host;
+        $this->dbname = $dbname;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    // создаём подключение к БД
+    public function getConnection()
+    {
+        $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset={$this->charset}";
+        try {
+            return new PDO($dsn, $this->username, $this->password, $this->options);
+        }catch (PDOException $e){
+            echo "DB error: " . $e->getMessage();
+            die();
+        }
+    }
+}
